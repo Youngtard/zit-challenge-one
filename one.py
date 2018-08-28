d07bd27a7b5c31b5d0098a72d73a64d557b531c5
youngtard <tolusotonwa@yahoo.com>
2018-08-28 09:51:41 +0100

    Some code refctoring

--- one.py
+++ one.py
@@ -1,26 +1,21 @@
-asterik = "*"
-maxAsteriksToBePrinted = 5
-numberOfAsteriksPrinted = 0
-currentNumberOfAsteriksToPrint = 1
+asterisk = "*"
+maxAsterisksToBePrinted = 5
+currentNumberOfAsterisksToPrint = 1
+numberOfAsterisksPrinted = 0
 
-for i in range(maxAsteriksToBePrinted):
-    print(asterik * currentNumberOfAsteriksToPrint)
-    currentNumberOfAsteriksToPrint += 1
-    numberOfAsteriksPrinted += 1
+for i in range(maxAsterisksToBePrinted):
+    print(asterisk * currentNumberOfAsterisksToPrint)
+    currentNumberOfAsterisksToPrint += 1
+    numberOfAsterisksPrinted += 1
 
-    if numberOfAsteriksPrinted == maxAsteriksToBePrinted:
-        currentNumberOfAsteriksToPrint = 4
-        for i in range(currentNumberOfAsteriksToPrint):
-            print(asterik * currentNumberOfAsteriksToPrint)
-            currentNumberOfAsteriksToPrint -= 1
+    # Outer loop iterates 5 times printing out the relevant number of asterisk(s)
+    # At the first four iterations, the enclosed if statement doesn't execute because it's condition evaluates to false
+    # At the fifth iteration, the if statement is now executed because the condition is now true (asterisks printed
+    # equals the maximum to be printed per iteration). Therefore, the nested loop can now run printing the remaining
+    # asterisks in reverse order
 
-
-
-
-
-
-
-
-
-
-
+    if numberOfAsterisksPrinted == maxAsterisksToBePrinted:
+        currentNumberOfAsterisksToPrint = 4
+        for j in range(currentNumberOfAsterisksToPrint):
+            print(asterisk * currentNumberOfAsterisksToPrint)
+            currentNumberOfAsterisksToPrint -= 1
--- third.py
+++ third.py
@@ -1,21 +1,21 @@
-def sqrtBin():
+def sqr_zeros():
     print("Enter a positive integer, then press Enter: ")
-    inputtedValue = input()
-    inputtedValue.isnumeric()
-    if inputtedValue.isdigit():
-        number = int(inputtedValue)
-        binary = bin(number)
+    inputted_value = input()
 
+    if inputted_value.isnumeric():
+        decimal_number = int(inputted_value)
+        binary_number = bin(decimal_number)
+        binary_string = str(binary_number)
 
-        binaryString = str(binary)
+        string_binary = binary_string[slice(2, len(binary_number))]
+        print("Binary value:", string_binary)
 
-        stringBinary = binaryString[slice(2, len(binary))]
-        print(stringBinary)
+        occurrences = string_binary.count("0")
+        print("Occurrences of zero:", occurrences)
 
-        occurences = stringBinary.count("0")
-        print(occurences)
-        print(occurences ** 2)
+        print("Square of occurrences:", occurrences ** 2)
     else:
-        print("Not an integer")
+        print("Inputted value is not a numeric value")
 
-sqrtBin()
+
+sqr_zeros()
